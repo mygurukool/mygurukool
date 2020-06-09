7a68950162b6b36bbfb2eed20628ad4def14bd57
stephane <stephane@paperlib.com>
2020-06-09 20:00:26 +0200

    enable logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import React, { Component, Fragment } from "react";
-import "..//App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, {Component, Fragment} from 'react';
+import '..//App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default class Header extends Component {
   render() {
     return (
@@ -9,21 +9,19 @@ export default class Header extends Component {
           <div className="container">
             <a className="navbar-brand mr-0 mr-md-2 logo">My Gurukool</a>
             <ul className="navbar-nav ml-md-auto">
-              <li className="nav-item">
-                <a
-                  className="far fa-user"
-                  href="#?"
-                  //onClick=
-                >
-                  <li>Signout</li>
-                </a>
-              </li>
+              <li className="nav-item"><i className="far fa-user"></i>&nbsp;<a href="#" onClick={(e) => {
+                  e.preventDefault();
+                  localStorage.removeItem('token');
+                  window.location.href =
+                  "https://login.microsoftonline.com/common/oauth2/v2.0/logout?post_logout_redirect_uri=" +
+                  process.env.REACT_APP_OAUTH_LOGOUT_URI;
+                }}>Logout</a></li>
               <li></li>
               <li></li>
             </ul>
           </div>
         </header>
       </Fragment>
-    );
+    )
   }
 }
