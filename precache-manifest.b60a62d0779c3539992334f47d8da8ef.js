68aed29627f37d35c04da48ef5cc23602b47acfa
NaveenB <naveen.bandhu@gmail.com>
2020-06-19 17:41:54 +0200

    Updates

---
+++ precache-manifest.b60a62d0779c3539992334f47d8da8ef.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "256978ac38cf56ced249",
+    "url": "naveenb.github.io/mygurukool/static/css/main.a60e1ccb.chunk.css"
+  },
+  {
+    "revision": "256978ac38cf56ced249",
+    "url": "naveenb.github.io/mygurukool/static/js/main.3c284cb6.chunk.js"
+  },
+  {
+    "revision": "f3732aa0f75d3c4dfb27",
+    "url": "naveenb.github.io/mygurukool/static/js/runtime~main.0af1cbbc.js"
+  },
+  {
+    "revision": "493204b411e5db6fbacd",
+    "url": "naveenb.github.io/mygurukool/static/css/2.5a531886.chunk.css"
+  },
+  {
+    "revision": "493204b411e5db6fbacd",
+    "url": "naveenb.github.io/mygurukool/static/js/2.128ff4f7.chunk.js"
+  },
+  {
+    "revision": "d947289c97aeb4d66c66a27e286246a1",
+    "url": "naveenb.github.io/mygurukool/static/media/maths.d947289c.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "naveenb.github.io/mygurukool/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "naveenb.github.io/mygurukool/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "adcf2d2a7ab767ccdec307f961cb84f4",
+    "url": "naveenb.github.io/mygurukool/index.html"
+  }
+];
