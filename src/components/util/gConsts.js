26c45ade91895b2a0a0f4d58becbd8d336dfb8ee
NaveenB <naveen.bandhu@gmail.com>
2020-06-29 14:25:38 +0200

    deleting additional scopes

--- src/components/util/gConsts.js
+++ src/components/util/gConsts.js
@@ -2,7 +2,7 @@
 
 //Application related constants
 export const REACT_APP_GOOGLE_OAUTH_SCOPES =
-  "https://www.googleapis.com/auth/userinfo.profile%20https://www.googleapis.com/auth/classroom.courses.readonly%20https://www.googleapis.com/auth/classroom.coursework.me.readonly https://www.googleapis.com/auth/classroom.coursework.me https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/classroom.coursework.students.readonly";
+  "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.students.readonly";
 export const REACT_APP_GOOGLE_CLASSROOM_API =
   "https://classroom.googleapis.com/v1/";
 export const REACT_APP_GOOGLE_USERINFO_API =
