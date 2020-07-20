1c2e340a402788471c675574e36479c87c3b3691
mygurukool <mygurukool2020@gmail.com>
2020-07-20 14:56:24 +0200

    corrected status to COURSE_ACTIVE

--- src/components/util/gConsts.js
+++ src/components/util/gConsts.js
@@ -2,9 +2,9 @@
 
 //Application related constants
 export const REACT_APP_GOOGLE_OAUTH_SCOPES =
-  "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/classroom.rosters.readonly https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.me https://www.googleapis.com/auth/drive";
+  "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.me https://www.googleapis.com/auth/classroom.rosters.readonly https://www.googleapis.com/auth/drive";  
 
-export const REACT_APP_GOOGLE_OAUTH_TUTOR_SCOPES = "https://www.googleapis.com/auth/classroom.coursework.students.readonly";
+export const REACT_APP_GOOGLE_OAUTH_TUTOR_SCOPES = "https://www.googleapis.com/auth/classroom.coursework.students https://www.googleapis.com/auth/classroom.courses https://www.googleapis.com/auth/classroom.coursework.students";
 
 export const GOOGLE_USERINFO_API  = "https://www.googleapis.com/userinfo/v2/";
 
@@ -14,5 +14,5 @@ export const GOOGLE_DRIVE_API        = "https://www.googleapis.com/drive/v3/"
 export const GOOGLE_DRIVE_UPLOAD_API = "https://www.googleapis.com/upload/drive/v3/"
 
 //features/ functionality related constants
-export const COURSE_STATUS = "ACTIVE";
+export const COURSE_ACTIVE = "ACTIVE";
 export const COURSE_ARCHIVED = "ARCHIVED";
