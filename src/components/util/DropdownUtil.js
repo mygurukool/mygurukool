5701229cf33a99d01e455a0f4daa67e02f17f7cc
mygurukool <mygurukool2020@gmail.com>
2020-07-27 17:15:41 +0200

    added custom toggle option

--- src/components/util/DropdownUtil.js
+++ src/components/util/DropdownUtil.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
-import { DropdownButton, Dropdown } from "react-bootstrap";
+import { Dropdown } from "react-bootstrap";
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  <a
+    href=""
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    {children}
+    &#x25bc;
+  </a>
+));
+
 
 export default class DropdownUtil extends Component {
   constructor(props) {
     super(props);
+    this.state = ({customToggle:this.props.customToggle});
   }
 
   handleChange = (event) => {
@@ -12,15 +28,24 @@ export default class DropdownUtil extends Component {
 
   render() {
     return (
-      <DropdownButton
-      id="dropdown-basic-button"
-      title={this.props.dropdownTitleText}
-      onSelect={(e) => this.handleChange(e)}
-      >
-       {this.props.itemList && this.props.itemList.map((item)=> {
-           return <Dropdown.Item eventKey={item}><b>{item}</b></Dropdown.Item>;
-       })}
-      </DropdownButton>
+    <Dropdown onSelect={(e) => this.handleChange(e)}>
+      {this.state.customToggle && this.state.customToggle === true 
+      ?(
+        <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+        {this.props.dropdownTitleText}
+        </Dropdown.Toggle>
+      ):(
+        <Dropdown.Toggle id="dropdown-custom-components"> 
+        {this.props.dropdownTitleText}
+        </Dropdown.Toggle>
+      )
+      }
+      <Dropdown.Menu>
+      {this.props.itemList && this.props.itemList.map((item)=> {
+        return <Dropdown.Item key={item} eventKey={item}><b>{item}</b></Dropdown.Item>;
+      })}
+      </Dropdown.Menu>
+    </Dropdown>
     );
   }
 }
