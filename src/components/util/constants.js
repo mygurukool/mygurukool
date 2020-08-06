2c7bfa59d1d9786cf2e9986f3f94efc5b69aba0f
mygurukool <mygurukool2020@gmail.com>
2020-08-06 21:49:35 +0200

    creating seperate js for teacherAuthorization

--- src/components/util/constants.js
+++ src/components/util/constants.js
@@ -3,3 +3,5 @@ export const GOOGLE = "Google";
 export const LOGIN_PROVIDER = "loginProvider";
 export const ACCESS_TOKEN = "token";
 export const MESSAGE_WINDOW_TIMEOUT = 5 * 60 * 1000;
+
+export const HAS_TEACHER_ACCEPTED = "hasTeacherAccepted"
