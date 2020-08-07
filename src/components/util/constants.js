98b70de3c3914c53a25f4038baf7b2ffc5f1756d
mygurukool <mygurukool2020@gmail.com>
2020-08-07 02:40:10 +0200

    replacing text with constants

--- src/components/util/constants.js
+++ src/components/util/constants.js
@@ -4,4 +4,6 @@ export const LOGIN_PROVIDER = "loginProvider";
 export const ACCESS_TOKEN = "token";
 export const MESSAGE_WINDOW_TIMEOUT = 5 * 60 * 1000;
 
-export const HAS_TEACHER_ACCEPTED = "hasTeacherAccepted"
+export const HAS_TEACHER_ACCEPTED = "hasTeacherAccepted";
+export const COURSE_ID = "COURSE_ID";
+export const DEFAULT_GROUP_NAME = 'undefined'
