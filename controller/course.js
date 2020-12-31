a92b10b3a419b23856352290af3beb090dbfc5a5
Nagesh <rao.nageshwar80@gmail.com>
2020-12-31 13:15:53 +0530

    All the CURD operation with some changes

--- controller/course.js
+++ controller/course.js
@@ -1,7 +1,21 @@
 const Course = require('../model/course')
 
-exports.test1 = (req, res) => {
-    console.log('req.body', req.body)
+exports.courseById = (req,res, next, id) => {
+    Course.findById(id).exec((err, course) => {
+        if(err || !course) {
+            return res.status(400).json({
+                error: 'course does not exist'
+            })
+        }
+        req.course = course;
+        next();
+    })
+    
+}
+
+
+exports.create = (req, res) => {
+    // console.log('req.body', req.body)
     const course = new Course(req.body)
     course.save((err, course) => {
         if(err) {
@@ -14,4 +28,48 @@ exports.test1 = (req, res) => {
             course
         })
     })
+}
+
+exports.get = (req, res) => {
+    return res.json(req.course)
+}
+
+exports.update = (req, res) => {
+    const course = req.course
+    course.id = req.body.id
+    course.name = req.body.name
+    course.section = req.body.section
+    course.descriptionHeading = req.body.descriptionHeading
+    course.description = req.body.description
+    course.room = req.body.room
+    course.ownerId = req.body.ownerId
+    course.creationTime = req.body.creationTime
+    course.updateTime = req.body.updateTime
+
+    course.save((err, data) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'not updated'
+            })
+        }
+        res.json(data)
+    })
+
+}
+
+// since we are using remove as delete is an inbuit word for javascript
+
+exports.remove = (req, res) => {
+    const course = req.course
+    course.remove((err, data) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'course could not be deleted'
+            })
+        }
+        res.json({
+            message: 'Course deleted'
+        })
+    })
+
 }
--- routes/course.js
+++ routes/course.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { test1 } = require('../controller/course')
+const { create, courseById, get, update, remove} = require('../controller/course')
 
-router.post('/course1', test1)
+router.get('/courses/:courseId', get)
+router.post('/courses', create) // teacher access
+router.put('/courses/:courseId', update)// teacher access
+router.delete('/courses/:courseId', remove)// teacher access
+
+
+router.param('courseId', courseById)
 
 module.exports = router;
