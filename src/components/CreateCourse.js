a2412c977d37924b2be1a1dc152d5c38b8a94e8c
mygurukool <mygurukool2020@gmail.com>
2021-01-04 20:56:58 +0100

    converting CreateCourse into a Modal screen

--- src/components/CreateCourse.js
+++ src/components/CreateCourse.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Modal, Form, Button } from "react-bootstrap";
 import * as _classworkUtil from "./google/ClassworkUtil";
 
 export default class CreateCourse extends React.Component {
@@ -8,6 +8,7 @@ export default class CreateCourse extends React.Component {
     this.state = {
       className: "",
       courseName: "",
+      showCourseModal: true,
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -44,38 +45,51 @@ export default class CreateCourse extends React.Component {
     const { className, courseName } = this.state;
     return (
       // {"create course"}
-      <div className="container">
-        <div className="card card-body">
-          <Form>
-            <Form.Label><b>Create Course</b></Form.Label>
-            <Form.Group controlId="formCreateCourse">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="courseName"
-                name="courseName"
-                placeholder="eg: English, French"
-                onChange={(e) => this.handleChange(e)}
-              />
-              {/* <Form.Text className="text-muted">
+      <Modal
+        show={this.state.showCourseModal}
+        backdrop="static"
+        keyboard={false}
+        centered={true}
+      >
+         <div className="container">
+          <div className="card card-body">
+            <Form>
+              <Form.Label>
+                <b>Create Course</b>
+              </Form.Label>
+              <Form.Group controlId="formCreateCourse">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="courseName"
+                  name="courseName"
+                  placeholder="eg: English, French"
+                  onChange={(e) => this.handleChange(e)}
+                />
+
+                {/* <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text> */}
 
-              <Form.Label>Class Name</Form.Label>
-              <Form.Control
-                type="className"
-                name="className"
-                placeholder="Name of the Class the Course should belong to. eg: Class 3A"
-                onChange={(e) => this.handleChange(e)}
-              />
-            </Form.Group>
-            <Button variant="secondary" type="cancel">
-              Cancel
-            </Button>
-            <Button variant="primary" type="submit" onClick={this.handleClick}>
-              Create
-            </Button>
-          </Form>
-          {/* <button
+                <Form.Label>Class Name</Form.Label>
+                <Form.Control
+                  type="className"
+                  name="className"
+                  placeholder="Name of the Class the Course should belong to. eg: Class 3A"
+                  onChange={(e) => this.handleChange(e)}
+                />
+              </Form.Group>
+              <Button variant="secondary" type="cancel">
+                Cancel
+              </Button>
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={this.handleClick}
+              >
+                Create
+              </Button>
+            </Form>
+            {/* <button
             type="button"
             className="btn btn-primary turnin"
            // disabled={!this.props.isActive}
@@ -83,8 +97,9 @@ export default class CreateCourse extends React.Component {
           >
             Close
           </button> */}
+          </div>
         </div>
-      </div>
+      </Modal>
     );
   }
 }
