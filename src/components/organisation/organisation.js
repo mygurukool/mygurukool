f6ab6c027a645c5672beb13b4375f84540f7ea14
rohit gupta <rohituideveloper99@gmail.com>
2021-10-13 12:45:33 +0530

    fixed subject icon issue and added css for form

--- src/components/organisation/organisation.js
+++ src/components/organisation/organisation.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from "react";
 import PageBanner from '../../assets/bg.jpg'
 import Header from "../Header";
+import "../../scss/comman.scss"
+import { ReactComponent as RightArrowIcon } from '../../assets/icons/arrow.svg'
 
 
 
@@ -35,22 +37,22 @@ export default class Organisation extends React.Component{
 
                       <div className="row">
                           <div className="col-md-6 mt-2">
-                              <input name="first_name" className="form-control" placeholder="My Name"/>
+                              <input name="first_name" className="form-control input-field" placeholder="My Name"/>
                           </div>
                           <div className="col-md-6 mt-2">
-                              <input name="username" className="form-control" placeholder="My Login Name"/>
+                              <input name="username" className="form-control input-field" placeholder="My Login Name"/>
                           </div>
                           <div className="col-md-6 mt-2">
-                              <input name="password" className="form-control" placeholder="Enter Password"/>
+                              <input name="password" className="form-control input-field" placeholder="Enter Password"/>
                           </div>
                           <div className="col-md-6 mt-2">
-                              <input className="form-control" placeholder="Reapeat Password"/>
+                              <input className="form-control  input-field" placeholder="Reapeat Password"/>
                           </div>
                           <div className="col-md-6 mt-2">
-                              <input name="orgName" className="form-control" placeholder="My Organisation"/>
+                              <input name="orgName" className="form-control input-field" placeholder="My Organisation"/>
                           </div>
                           <div className="col-md-6 mt-2">
-                              <select className="form-control" name="orgSize" placeholder="size">
+                              <select className="form-control  input-field" name="orgSize" placeholder="size">
                                   {
                                       size.map((value,key)=>(
                                           <option value={value.value} key={key}>{value.name}</option>
@@ -69,16 +71,19 @@ export default class Organisation extends React.Component{
                                </select>*/
                              }
 
-                              <select className="form-control" name="orgCountry" placeholder="size">
+                              <select className="form-control input-field" name="orgCountry" placeholder="size">
                               <option value="in">India </option>
                               <option value="us">US </option>
                             </select>
                           </div>
                           <div className="col-md-12 mt-2">
-                              <textarea name="orgAddress" className="form-control" placeholder="address"></textarea>
+                              <textarea name="orgAddress" className="form-control input-field" placeholder="address"></textarea>
                           </div>
-                          <div className="col-md-12 mt-2 text-center">
-                              <button className="btn btn-primary">Create</button>
+                          <div className="col-md-12 mt-2 text-center create-btn">
+                              <button className="btn-red bg-green txt-white">
+                                  Create
+                                  <RightArrowIcon />
+                              </button>
                           </div>
                       </div>   
                       </form>
