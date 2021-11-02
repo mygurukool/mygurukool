7a984a497cf5525acb477a229f1c4c4a22ae6bf0
mygurukool <mygurukool2020@gmail.com>
2021-11-02 08:40:35 +0100

    preparing for sync with socket.io impl

--- src/components/communication/Comments.js
+++ src/components/communication/Comments.js
@@ -1,9 +1,41 @@
 import React, { Component, Fragment } from "react";
 import { MessageBox, MessageList, Input, Button } from "react-chat-elements";
+import * as _apiUtils from "../util/AxiosUtil";
 import "react-chat-elements/dist/main.css";
 import * as _constant from "../util/constants";
-import * as _apiUtils from "../util/AxiosUtil";
 
+let initiatorMsg = {
+  position: "right",
+  replyButton: true,
+  type: "text",
+  theme: "white",
+  view: "list",
+  title: "From: MyGuruKool Student",
+  text: "How do I solve this issue",
+  status: "read",
+  date: +new Date(),
+  onReplyMessageClick: () => {
+    console.log("onReplyMessageClick");
+    alert("onReplyMessageClick");
+  },
+};
+
+//this field is an individual message from 2nd person, be a teacher or another student
+let responseMsg = {
+  position: "left",
+  replyButton: true,
+  type: "text",
+  theme: "white",
+  view: "list",
+  title: "From: Teacher",
+  text: "Read through Chapter 1",
+  status: "sent",
+  date: +new Date(),
+  onReplyMessageClick: () => {
+    console.log("onReplyMessageClick");
+    alert("onReplyMessageClick");
+  },
+};
 
 const REPLY = "Reply";
 const ASK = "Ask";
@@ -11,167 +43,32 @@ export default class Comments extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showMessageBlock: false,
-      show: true,
-      messageList: [], //  [studentMsg, teacherMsg], //TODO: Dev hack, msglist tobe del for prod
-      gDriveCommentsFileId: null,
-      commentButtonText: this.props.user.isTeacherLogin? 'Send comments to Students' : 'Feel free to ask!!',
+      showCommentBlock: false,
+      commentList: [initiatorMsg, responseMsg], //TODO: Dev hack, this commentList should be coming from/ sync with the server
     };
+    // this.cancelClick = this.cancelClick.bind(this);
     this.showInputElement = this.showInputElement.bind(this);
     this.startTimeOut = this.startTimeOut.bind(this);
-    this.syncComments = this.syncComments.bind(this);
-    this.mergeComments = this.mergeComments.bind(this);
-    this.syncWithServer = this.syncWithServer.bind(this);
-    this.input = React.createRef();
   }
 
-  closeComment = () => {
+  cancelClick = (event) => {
     this.setState({
-      showMessageBlock: false,
+      showCommentBlock: false,
       showReplyBlock: false,
     });
-    this.syncComments();
-  };
-
-  openComments = () => {
-    this.syncComments();
-    this.setState({
-      showMessageBlock: true,
-    });
+    console.log(this.state.commentList);
   };
 
   startTimeOut() {
     // this.props.resetTimeout(true);
     setTimeout(() => {
-      this.setState({ showMessageBlock: false });
+      this.setState({ showCommentBlock: false });
     }, _constant.MESSAGE_WINDOW_TIMEOUT);
     return false;
   }
 
-  componentWillMount() {
-    // setInterval(this.addMessage.bind(this), 3000);
-  }
-
-  mergeComments(localList, serverList) {
-    const mergedArray = [...serverList, ...localList];
-    // mergedArray have duplicates, remove the duplicates using Set
-    let set = new Set();
-    let unionArray = mergedArray.filter((item) => {
-      if (!set.has(item.date)) {
-        set.add(item.date);
-        return true;
-      }
-      return false;
-    }, set);
-
-    let sortedList = unionArray.sort(function (a, b) {
-      return a.date - b.date;
-    });
-    return sortedList;
-  }
-
-  syncWithServer(localMsgs, fileName, fileId) {
-    let downloadedComments;
-    //fetch downloadable file
-    _apiUtils.googleDriveDownloadFile(fileId).then((resJsonFile) => {
-      downloadedComments = resJsonFile.data;
-
-      //merge locallist with serverlist update this.state.messageList
-      this.setState({ messageList: this.mergeComments(localMsgs, downloadedComments) });
-
-      //update server with mergedList
-      _apiUtils
-        .googleDriveUpdateFile(
-          fileName,
-          JSON.stringify(this.state.messageList),
-          "application/json",
-          fileId
-        )
-        .then((response) => {})
-        .catch((error) => {
-          console.log("syncWithServer.googleDriveUpdateFile " + error);
-        });
-    }).catch((error) => {
-      console.trace("syncWithServer.googleDriveDownloadFile " + JSON.stringify(error));
-    });
-  }
-
-  syncComments() {
-    let fileName = this.props.subjectName.trim() + "_CommentTranscript.json";
-
-    //update the file else get file by Name
-    if (this.state.gDriveCommentsFileId) {
-      /*
-        This section will be executed(actions: fetch, merge and update)
-          - For both read and write as soon as 
-              1: Comments file is created 
-              2: After first fetch for the current session
-      */
-
-      this.syncWithServer(
-        this.state.messageList,
-        fileName,
-        this.state.gDriveCommentsFileId
-      );
-    } else {
-      /*
-      This clause will be executed(actions: fetch by file name, merge and update)
-        1: Comments file doesnt exist 
-        2: First fetch for the current session
-      */
-
-      //Step2: get file by Name  ..if it doesnt exist Create else update
-      _apiUtils
-        .googleDriveGetFiles({
-          fields: "*",
-          q: `name = '${fileName}' and trashed = false`,
-        })
-        .then((respByName) => {
-          if (respByName.data.files.length === 0) {
-            console.log("File doesnt exists: " + fileName)
-            //Step1: get StudentCourseDetails, tobe able to get studentWorkFolder.id
-            // _apiUtils
-            //   .googleClassroomCourseStudentsList(this.props.courseId)
-            //   .then((respCourseDetails) => {
-            //     //Step2 create
-            //     _apiUtils
-            //       .googleDriveUploadFile(
-            //         fileName,
-            //         JSON.stringify(this.state.messageList),
-            //        // this.state.messageList,
-            //         //"application/json",
-            //         "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-            //         respCourseDetails.data.studentWorkFolder.id
-            //       )
-            //       .then((resCreate) => {
-            //         this.setState({
-            //           //set gdrive file id
-            //           gDriveCommentsFileId: resCreate.data.id,
-            //         });
-            //       })
-            //       .catch((error) => {
-            //         console.log("Create File " + error);
-            //       });
-            //   });
-          } else {
-            //***update section***
-            let fileId;
-            if (respByName.data.hasOwnProperty("id"))
-              fileId = respByName.data.id;
-            else fileId = respByName.data.files[0].id;
-
-            this.setState({
-              //set gdrive file id
-              gDriveCommentsFileId: fileId,
-            });
-            //sync comment with server
-            this.syncWithServer(this.state.messageList, fileName, fileId);
-          }
-        })
-        .catch((error) => {
-          console.log("GetByFileName " + error);
-        });
-    }
+  componentDidMount() {
+    // this.startTimeOut();
   }
 
   getRandomColor() {
@@ -183,7 +80,8 @@ export default class Comments extends Component {
     return color;
   }
 
-  buildMessage(message, msgType) {
+  buildComment(comment, msgType) {
+    //alert("buildComment  " + this.state.replyEventObj);
     let msg = {
       reply: msgType === REPLY ? this.state.replyEventObj : null,
       position: "right", //"right" : "left",
@@ -192,9 +90,9 @@ export default class Comments extends Component {
       type: "text",
       theme: "white",
       view: "list",
-      title: "Author name: " + this.props.user.name,
+      title: "Author name: " + this.props.userName,
       //titleColor: this.getRandomColor(),
-      text: message,
+      text: comment,
       status: "read",
       date: +new Date(),
       onReplyMessageClick: () => {
@@ -206,11 +104,62 @@ export default class Comments extends Component {
     return msg;
   }
 
-  addMessage(message, msgType) {
-    var list = this.state.messageList;
-    list.push(this.buildMessage(message, msgType));
+  // temparory function, need to evaluate should function be required, based on how the server is sending the text 
+  buildTeacherComment(comment, msgType) {
+    //alert("buildComment  " + this.state.replyEventObj);
+    let msg = {
+      reply: msgType === REPLY ? this.state.replyEventObj : null,
+      position: "left", //"right" : "left",
+      // forwarded: true,
+      replyButton: true,
+      type: "text",
+      theme: "white",
+      view: "list",
+      title: "Author name: Teacher",
+      //titleColor: this.getRandomColor(),
+      text: "teachers reply for your comment: " + comment,
+      status: "read",
+      date: +new Date(),
+      onReplyMessageClick: () => {
+        console.log("onReplyMessageClick");
+        alert("onReplyMessageClick");
+      },
+      // avatar: `data:image/png;base64,${this.photo()}`,
+    };
+    return msg;
+  }
+
+  syncComments() {
+    _apiUtils
+      .googleDriveGetFiles({
+        fields: "*",
+        q: `name = 'Mathematics Class/ Class teacher name'`,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+    //googleDriveUploadFile(name, content, mime, folderId)
+    alert("Submit Clicked: " + this.state.courseId);
+    // let folderId =
+    //   "0BzJfQgFkF_jafjRtSDUxazlqaVFtVVZLQ2ZPX1dMaFNzektPZ293WW5IOWlHU0VfODdWcjg";
+    // _apiUtils
+    //   .googleDriveUploadFile(
+    //     this.file.name,
+    //     this.file,
+    //     "application/octet-stream",
+    //     folderId
+    //   )
+    //   .then((response) => {
+    //     console.log(response);
+    //   });
+  }
+
+  addComment(comment, msgType) {
+    var list = this.state.commentList;
+    list.push(this.buildComment(comment, msgType));
+    list.push(this.buildTeacherComment(comment, msgType));
     this.setState({
-      messageList: list,
+      commentList: list,
     });
     if (msgType === REPLY) {
       this.setState({
@@ -222,11 +171,11 @@ export default class Comments extends Component {
   /* 
   Method to display Input field
   Type: Text on Button, type of input field:
-       "Send" (Direct message) / "Reply" (Comment existing msg)
+       "Send" (Direct comment) / "Reply" (Comment existing msg)
   */
   showInputElement(type) {
     this.startTimeOut();
-    let message;
+    let comment;
     let defaultPlaceHolder = type === REPLY ? "Reply..." : "Type here...";
     let inputCssName = type === ASK ? "card col-12" : "card row-12";
     let replyCssName = type === REPLY ? "card row-12" : "";
@@ -234,11 +183,15 @@ export default class Comments extends Component {
       <Fragment>
         <br />
         <div className={replyCssName}>
-          {type === REPLY ? (
-            <MessageBox reply={this.state.replyEventObj} position={"right"} />
-          ) : (
-            ""
-          )}
+          {type === REPLY
+            ? (alert("buildingReply: " + this.state.replyEventObj.comment),
+              (
+                <MessageBox
+                  reply={this.state.replyEventObj}
+                  position={"right"}
+                />
+              ))
+            : ""}
 
           {/* <div className="row"> */}
           <div className={inputCssName}>
@@ -246,21 +199,20 @@ export default class Comments extends Component {
               placeholder={defaultPlaceHolder}
               defaultValue=""
               ref={this.input}
-              multiline={false}
+              multiline={true}
               autofocus={true}
               // buttonsFloat="left"
               onKeyPress={(e) => {
                 this.startTimeOut();
-                message = e.target.value;
-                if (e.keyUp && e.shiftKey && e.charCode === 13) {
+                comment = e.target.value;
+                if (e.shiftKey && e.charCode === 13) {
                   this.input.current.clear();
                   return true;
                 }
                 if (e.charCode === 13) {
-                  // this.refs.input &&
-                  message = e.target.value;
                   this.input.current.clear();
-                  this.addMessage(message, type);
+                  this.addComment(comment, type);
+                  e.preventDefault();
                   return false;
                 }
               }}
@@ -268,17 +220,17 @@ export default class Comments extends Component {
                 type === ASK ? (
                   <Button
                     text={type}
-                    onClick={() => this.addMessage(message, type)}
+                    onClick={(e) => this.addComment(comment, type)}
                   />
                 ) : (
                   <div className="col-2">
                     <Button
                       text={type}
-                      onClick={() => this.addMessage(message, type)}
+                      onClick={(e) => this.addComment(comment, type)}
                     />{" "}
                     <Button
                       text="Cancel"
-                      onClick={() =>
+                      onClick={(e) =>
                         this.setState({
                           showReplyBlock: false,
                         })
@@ -296,7 +248,7 @@ export default class Comments extends Component {
       </Fragment>
     );
   }
-  replyMessage = (e) => {
+  replyComment = (e) => {
     console.log("onReplyClick " + JSON.stringify(e));
     this.setState({
       showReplyBlock: true,
@@ -304,9 +256,10 @@ export default class Comments extends Component {
         // photoURL: "https://facebook.github.io/react/img/logo.svg",
         title: e.title,
         titleColor: "#8717ae",
-        message: e.text,
+        comment: e.text,
       },
     });
+    alert("replyComment " + e.text);
   };
   render() {
     return (
@@ -314,12 +267,11 @@ export default class Comments extends Component {
         <button
           type="button"
           className="btn btn-primary turnin"
-          onClick={this.openComments}
-          disabled={!this.props.isActive}
+          onClick={() => this.setState({ showCommentBlock: true })}
         >
-          <i className="fas fa-question-circle"></i> {this.state.commentButtonText}
+          <i className="fas fa-question-circle"></i> Feel free to ask!!
         </button>
-        {this.state.showMessageBlock === true ? (
+        {this.state.showCommentBlock === true ? (
           <div className="card card-body fileblock col-12">
             <div className="container">
               <div className="right-panel">
@@ -327,8 +279,8 @@ export default class Comments extends Component {
                   className="message-list"
                   lockable={true}
                   downButtonBadge={10}
-                  dataSource={this.state.messageList}
-                  onReplyClick={(e) => this.replyMessage(e)}
+                  dataSource={this.state.commentList}
+                  onReplyClick={(e) => this.replyComment(e)}
                   replyButton={true}
                 />
                 {/* Show the Reply Block, only when Reply is Clicked */}
@@ -340,10 +292,10 @@ export default class Comments extends Component {
             <div className="form-group">
               <button
                 type="reset"
-                onClick={this.closeComment}
+                onClick={this.cancelClick}
                 className="btn btn-danger float-right"
               >
-                <i className="fas fa-comment-slash"></i> Close Interaction
+                <i className="far fa-times-circle"></i> Close
               </button>
             </div>
           </div>
